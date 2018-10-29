17de88871f046f3f46d30de59be217d9f28ca9b9
Sergey Davydov <prozerox2@yandex.ru>
2018-10-29 22:54:37 +0300

    rank delete

--- Resolvers/MultirankArrayResolver.cs
+++ Resolvers/MultirankArrayResolver.cs
@@ -10,8 +10,8 @@ namespace SerializeMachine.Resolvers
     {
         private const string XML_NAME_DIMENTION_CONTAINER = "_";
         private const string XML_ATTRIBUTE_DIMENSION = "DIMENSION";
-        private const string XML_ATTRIBUTE_RANK = "RANK";
 
+        public readonly int Rank;
         public readonly Type ElementType;
         public readonly Serializator Serializator;
 
@@ -19,13 +19,14 @@ namespace SerializeMachine.Resolvers
         {
             if (!resolveType.IsArray) throw new ArgumentException($"MultirankArrayResolver. Initializing was failed: {nameof(resolveType)} is not array");
             ElementType = resolveType.GetElementType();
+            Rank = resolveType.GetArrayRank();
             Serializator = serializator ?? throw new ArgumentNullException($"MultirankArrayResolver. Initializing was failed: ");
         }
 
         public override void Deserialzie(XElement serialized, ref object instance)
         {
             var resolveArray = instance as Array;
-            var indexator = new int[resolveArray.Rank];
+            var indexator = new int[Rank];
 
             DimensionDeresolve(serialized.Element(XML_NAME_DIMENTION_CONTAINER), resolveArray, indexator, indexator.Length - 1);
         }
@@ -33,34 +34,20 @@ namespace SerializeMachine.Resolvers
         public override void Serialize(XElement serialized, object instance)
         {
             var resolveArray = instance as Array;
-            var indexator = new int[resolveArray.Rank];
+            var indexator = new int[Rank];
 
             DimensionResolve(serialized, resolveArray, indexator, indexator.Length - 1);
 
-            SetRank(serialized, resolveArray.Rank);
             SetDimensions(serialized, resolveArray);
         }
 
         protected internal override object GetTemplateInstance(XElement serializedObject)
         {
-            var rank = GetRank(serializedObject);
-            var dimensions = GetDimensions(serializedObject, rank);
+            var dimensions = GetDimensions(serializedObject, Rank);
 
             return Array.CreateInstance(ElementType, dimensions);
         }
 
-        int GetRank(XElement serializedObject)
-        {
-            var rankAttribute = serializedObject.Attribute(XML_ATTRIBUTE_RANK);
-            if (rankAttribute == null)
-                throw new SerializationException($"MultirankArrayResolver. Rank the array read was failed: attribute <{XML_ATTRIBUTE_RANK}> not contains");
-
-            int rank;
-            if (!int.TryParse(rankAttribute.Value, out rank))
-                throw new SerializationException($"MultirankArrayResolver. Rank the array read was failed: value <{XML_ATTRIBUTE_RANK}> is not 32-bit integer number");
-
-            return rank;
-        }
         int[] GetDimensions(XElement serializedObject, int rank)
         {
             var dimensions = new int[rank];
@@ -83,10 +70,6 @@ namespace SerializeMachine.Resolvers
             var rank = array.Rank;
             for (int i = 0; i < rank; i++)
                 serializedObject.SetAttributeValue(XML_ATTRIBUTE_DIMENSION + i.ToString(), array.GetLength(i).ToString());//MB USE INT RESOLVER?..
-        }
-        void SetRank(XElement serializedObject, int rank)
-        {
-            serializedObject.Add(new XAttribute(XML_ATTRIBUTE_RANK, rank.ToString()));
         }
 
         void DimensionResolve(XElement node, Array array, int[] indexator, int Dimension)
